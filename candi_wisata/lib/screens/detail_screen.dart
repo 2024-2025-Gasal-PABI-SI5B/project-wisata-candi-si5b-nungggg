71c60caa0f80b8f3b9e33d62377fe56cc0450c46
nungggg <nur937225@gmail.com>
2024-11-10 16:15:25 +0700

    c

--- candi_wisata/lib/screens/detail_screen.dart
+++ candi_wisata/lib/screens/detail_screen.dart
@@ -10,7 +10,41 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold (
       body: Column(
-        children: [],
+        children: [
+          //DETAIL HEADER 
+          //IMAGE UTAMA
+          Stack(
+            children: [
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  candi.imageAsset,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                  ),
+              ),
+              ),
+              //TOMBOL BACK CUSTOM
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape:  BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.arrow_back)
+                    ),
+                )
+                ),
+            ],
+          ),
+          //DETAIL INFO
+          //INFO ATAS
+          
+        ],
         )
     );
   }
